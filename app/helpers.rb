064e76ac1d4b15e3b820243c09802a282ba7385d
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2009-09-30 13:48:12 +0200

    Helper moved to Model, which is the one that uses it.

--- app/helpers.rb
+++ app/helpers.rb
@@ -1,16 +1,6 @@
 
 module Shrtr
   
-  module Helpers
-    SIXTYTWO = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.freeze
-    def to_s_62(i)
-      return SIXTYTWO[i] if i < 62
-      s = ''
-      while i > 0
-        s << SIXTYTWO[i.modulo(62)]
-        i /= 62
-      end
-      s.reverse
     end
   end
 
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,26 +1,23 @@
 require 'uri'
-require 'rufus/tokyo'
 
-module Shrtr
-  include URI::Escape
-  
-  class URL
+module Shrtr  
+  class URL    
     class << self
       attr :db
       
-      def connect!(conf)
-        @db = Rufus::Tokyo::Cabinet.new(File.join(ROOT_DIR, conf['name']))
+      def connect!(db)
+        @db ||= db
       end
       
       def find_by_url(url)
         url = "http://#{url}" unless /^[^:]+:\/\// =~ url
         
-        shrtr_id = @db["url:#{escape(url)}"]
+        shrtr_id = @db.execute { |db| db["url:#{URI.escape(url)}"] }
         new(url, shrtr_id)
       end
       
       def find_by_shrtr_id(shrtr_id)
-        url = @db["shrtr_id:#{shrtr_id}"]
+        url = @db.execute { |db| db["shrtr_id:#{shrtr_id}"] }
         new(url, shrtr_id) if url
       end
     end
@@ -31,10 +28,24 @@ module Shrtr
     def initialize(url, shrtr_id = nil)
       @url, @shrtr_id = url, shrtr_id
       if @shrtr_id.nil?
-        @shrtr_id = db.incr('shrtr_id-global:next_shrtr_id')
-        @db["shrtr_id:#{@shrtr_id}"] = @url
-        @db["url:#{escape(@url)}"] = @shrtr_id
+        @shrtr_id = to_s_62(Shrtr::URL.db.execute { |db| db.incr('shrtr_id-global:next_shrtr_id') })
+        Shrtr::URL.db.execute { |db| db["shrtr_id:#{@shrtr_id}"] = @url }
+        Shrtr::URL.db.execute { |db| db["url:#{URI.escape(@url)}"] = @shrtr_id }
       end
     end
+    
+    private
+    
+    SIXTYTWO = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.freeze
+    def to_s_62(i)
+      return SIXTYTWO[i,1] if i < 62
+      s = ''
+      while i > 0
+        s << SIXTYTWO[i.modulo(62)]
+        i /= 62
+      end
+      s.reverse
+    end
+    
   end
 end
